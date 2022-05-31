a65987011a4c0fdc803f5f6f5fa20489d5431d7f
若海 <wang@rehiy.com>
2022-06-01 02:04:39 +0800

    删除冗余组件

--- src/apps/user.vue
+++ src/apps/user.vue
@@ -9,13 +9,8 @@
             </div>
           </template>
           <div class="info">
-            <div class="info-image" @click="showDialog">
-              <img :src="avatarImg" />
-              <span class="info-edit">
-                <el-icon>
-                  <Camera />
-                </el-icon>
-              </span>
+            <div class="info-image">
+              <img src="@/assets/img/avatar.jpg" />
             </div>
             <div class="info-name">{{ name }}</div>
             <div class="info-desc">不可能！我的代码怎么可能会有bug！</div>
@@ -47,33 +42,13 @@
         </el-card>
       </el-col>
     </el-row>
-    <el-dialog title="裁剪图片" v-model="dialogVisible" width="600px">
-      <vue-cropper ref="cropper" :src="imgSrc" :ready="cropImage" :zoom="cropImage" :cropmove="cropImage"
-        style="width: 100%; height: 400px"></vue-cropper>
-      <template #footer>
-        <span class="dialog-footer">
-          <el-button class="crop-demo-btn" type="primary">
-            选择图片
-            <input class="crop-input" type="file" name="image" accept="image/*" @change="setImage" />
-          </el-button>
-          <el-button type="primary" @click="saveAvatar">上传并保存</el-button>
-        </span>
-      </template>
-    </el-dialog>
   </div>
 </template>
 
 <script>
 import { reactive, ref } from 'vue'
-import VueCropper from 'vue-cropperjs'
-import 'cropperjs/dist/cropper.css'
-
-import avatar from '@/assets/img/logo-min.png'
 
 export default {
-  components: {
-    VueCropper
-  },
   name: 'user',
   setup() {
     const name = localStorage.getItem('vt_username')
@@ -84,53 +59,10 @@ export default {
     })
     const onSubmit = () => { }
 
-    const avatarImg = ref(avatar)
-    const imgSrc = ref('')
-    const cropImg = ref('')
-    const dialogVisible = ref(false)
-    const cropper = ref(null)
-
-    const showDialog = () => {
-      dialogVisible.value = true
-      imgSrc.value = avatarImg.value
-    }
-
-    const setImage = (e) => {
-      const file = e.target.files[0]
-      if (!file.type.includes('image/')) {
-        return
-      }
-      const reader = new FileReader()
-      reader.onload = (event) => {
-        dialogVisible.value = true
-        imgSrc.value = event.target.result
-        cropper.value && cropper.value.replace(event.target.result)
-      }
-      reader.readAsDataURL(file)
-    }
-
-    const cropImage = () => {
-      cropImg.value = cropper.value.getCroppedCanvas().toDataURL()
-    }
-
-    const saveAvatar = () => {
-      avatarImg.value = cropImg.value
-      dialogVisible.value = false
-    }
-
     return {
       name,
       form,
-      onSubmit,
-      cropper,
-      avatarImg,
-      imgSrc,
-      cropImg,
-      showDialog,
-      dialogVisible,
-      setImage,
-      cropImage,
-      saveAvatar,
+      onSubmit
     }
   },
 }
@@ -143,7 +75,6 @@ export default {
 }
 
 .info-image {
-  position: relative;
   margin: auto;
   width: 100px;
   height: 100px;
@@ -158,47 +89,10 @@ export default {
   height: 100%;
 }
 
-.info-edit {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  opacity: 0;
-  transition: opacity 0.3s ease;
-}
-
-.info-edit i {
-  color: #eee;
-  font-size: 25px;
-}
-
-.info-image:hover .info-edit {
-  opacity: 1;
-}
-
 .info-name {
   margin: 15px 0 10px;
   font-size: 24px;
   font-weight: 500;
   color: #262626;
-}
-
-.crop-demo-btn {
-  position: relative;
-}
-
-.crop-input {
-  position: absolute;
-  width: 100px;
-  height: 40px;
-  left: 0;
-  top: 0;
-  opacity: 0;
-  cursor: pointer;
 }
 </style>
