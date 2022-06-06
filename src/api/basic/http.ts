3f3648f751b7ba77bc934584a6a9d448f373ee2e
若海 <wang@rehiy.com>
2022-06-06 11:48:28 +0800

    集中处理错误信息

--- src/api/basic/http.ts
+++ src/api/basic/http.ts
@@ -1,13 +1,12 @@
+import { ElMessage } from 'element-plus';
+
 export class HttpClient {
     protected async get(url: string) {
         const body = await fetch('/api' + url, {
             method: 'GET',
             headers: this.buildHeader(),
         });
-        const data = await body.json();
-        if (data.Payload) {
-            return data.Payload;
-        }
+        return this.parseResponse(body);
     }
 
     protected async post(url: string, input: Record<string | number, any>) {
@@ -16,17 +15,25 @@ export class HttpClient {
             headers: this.buildHeader('json'),
             body: JSON.stringify(input),
         });
-        const data = await body.json();
-        if (data.Payload) {
-            return data.Payload;
-        }
+        return this.parseResponse(body);
     }
 
     protected async delete(url: string) {
         const body = await fetch('/api' + url, {
             method: 'DELETE',
         });
+        return this.parseResponse(body);
+    }
+
+    protected async parseResponse(body: Response) {
         const data = await body.json();
+
+        if (data.Error) {
+            const msg = data.Error.message || '未知错误';
+            ElMessage.error(msg);
+            throw new Error(msg);
+        }
+
         if (data.Payload) {
             return data.Payload;
         }
