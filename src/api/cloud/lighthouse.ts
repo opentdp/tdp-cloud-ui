bcec695409d31b75a7cc760e6bb54112dd2f0a02
若海 <wang@rehiy.com>
2022-06-08 00:22:35 +0800

    新增流量查询接口

--- src/api/cloud/lighthouse.ts
+++ src/api/cloud/lighthouse.ts
@@ -1,11 +1,23 @@
 import { HttpClient } from '../basic/http';
 
 export class LighthouseClient extends HttpClient {
+    public describeRegions() {
+        return this.get('/cloud/lighthouse/describeRegions');
+    }
+
+    public describeInstances(region: string) {
+        return this.get('/cloud/lighthouse/describeInstances/' + region);
+    }
+
     public describeRegionsInstances() {
         return this.get('/cloud/lighthouse/describeRegionsInstances');
     }
 
-    public describeInstancesTrafficPackages(region: string) {
-        return this.get('/cloud/lighthouse/describeInstancesTrafficPackages/' + region);
+    public describeTrafficPackages(region: string) {
+        return this.get('/cloud/lighthouse/describeTrafficPackages/' + region);
+    }
+
+    public describeRegionTrafficPackages() {
+        return this.get('/cloud/lighthouse/describeRegionTrafficPackages');
     }
 }
