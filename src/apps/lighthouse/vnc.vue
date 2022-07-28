a9e4b8184979b46b20e763aa763cae3cfcaafaf6
若海 <wang@rehiy.com>
2022-07-28 23:58:53 +0800

    vnc 支持快捷命令

--- src/apps/lighthouse/vnc.vue
+++ src/apps/lighthouse/vnc.vue
@@ -14,7 +14,17 @@
                 VNC 终端
             </el-breadcrumb-item>
         </el-breadcrumb>
-        <iframe ref="frame" frameborder="no" />
+        <iframe ref="frame" frameborder="no" class="mgb10" />
+        <el-card shadow="hover">
+            <template #header>
+                <div class="card-header">
+                    <b>快捷命令</b>
+                </div>
+            </template>
+            <el-button v-for="item in cmdList" :key="item.id" @click="vncExec(item.cmd)">
+                {{ item.name }}
+            </el-button>
+        </el-card>
     </div>
 </template>
 
@@ -31,27 +41,57 @@ const zone = route.params.zone as string
 const region = zone.replace(/-\d$/, "")
 const instanceId = route.params.instanceId as string
 
+const vncExec = (cmd: string) => {
+    const ifrm = document.querySelector('iframe')
+    if (ifrm && ifrm.contentWindow) {
+        const vdoc = ifrm.contentWindow.document
+        const cbtn = vdoc.querySelector('a.copyBtn') as HTMLAnchorElement
+        cbtn.click()
+        setTimeout(() => {
+            const ipt = vdoc.querySelector('textarea.vnc-paste-text') as HTMLInputElement
+            const btn = vdoc.querySelector('button.bt_confirm') as HTMLInputElement
+            cmd = cmd.replace(/\n\s+/g, '\n')
+            cmd = cmd.replace(/\n+/g, '\n')
+            cmd = cmd.trim() + '\n'
+            ipt.value = cmd
+            btn.click()
+        }, 500)
+    }
+}
+
+const cmdList = [
+    {
+        id: 0,
+        name: "清理垃圾",
+        cmd: `
+            find /var/log -type f -name *.[0-9] -delete
+            find /var/log -type f -name *.gz -delete
+            find /var/log -type f -name *.xz -delete
+
+            > /var/log/wtmp
+            > /var/log/btmp
+            > /var/log/lastlog
+            > /var/log/faillog
+
+            > /root/.bash_history
+        `
+    }
+]
+
 onMounted(async () => {
     const data = await Api.lighthouse.describeInstanceVncUrl(region, {
         InstanceId: instanceId,
     })
-    const vnc = 'https://img.qcloud.com/qcloud/app/active_vnc/index.html?InstanceVncUrl='
     if (frame.value) {
+        const vnc = '/api/terminal/vnc?InstanceVncUrl='
         frame.value.src = vnc + data.InstanceVncUrl
     }
 })
 </script>
 
 <style lang="scss" scoped>
-.vnc-wrap {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 100%;
-}
-
 iframe {
     width: 100%;
-    flex: auto;
+    min-height: 860px;
 }
 </style>
