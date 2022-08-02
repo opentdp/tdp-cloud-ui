4b1140dbbffc5bba8b224cc5057da578fa3a5309
若海 <wang@rehiy.com>
2022-08-02 11:17:28 +0800

    配置文件中设置参数

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     },
     server: {
         port: 7900,
-        open: false,
+        open: true,
         proxy: {
             "/api": {
                 target: "http://127.0.0.1:7800",
