1e4bbd05a0d90a3b7c913f115707466a284e4acb
若海 <wang@rehiy.com>
2022-08-14 00:06:46 +0800

    优化列宽

--- src/apps/dnspod/index.vue
+++ src/apps/dnspod/index.vue
@@ -33,21 +33,21 @@ getDomainList()
             </template>
             <el-table :data="domainDescribe.DomainList" table-layout="fixed">
                 <el-table-column fixed prop="Name" label="域名" min-width="150" />
-                <el-table-column label="状态" min-width="100">
+                <el-table-column label="状态" min-width="80">
                     <template #default="scope">
                         {{ DomainStatusMap[scope.row.Status] }}
                     </template>
                 </el-table-column>
-                <el-table-column prop="RecordCount" label="记录数" min-width="100" />
+                <el-table-column prop="RecordCount" label="记录数" min-width="80" />
                 <el-table-column prop="EffectiveDNS" label="NS 服务器" min-width="250" />
-                <el-table-column label="NS 记录" min-width="100">
+                <el-table-column label="NS 记录" min-width="80">
                     <template #default="scope">
                         {{ scope.row.DNSStatus ? "错误" : "正常" }}
                     </template>
                 </el-table-column>
-                <el-table-column prop="GradeTitle" label="套餐" min-width="100" />
+                <el-table-column prop="GradeTitle" label="套餐" min-width="80" />
                 <el-table-column prop="VipEndAt" label="套餐有效期" min-width="180" />
-                <el-table-column fixed="right" label="操作" width="180" align="center">
+                <el-table-column fixed="right" label="操作" width="90" align="center">
                     <template #default="scope">
                         <el-button link type="primary" icon="View">
                             <router-link :to="'/dnspod/detail/' + scope.row.Name">
