77477ca47f571aad07d3304d41023fcb75b0bbdf
若海 <wang@rehiy.com>
2023-02-01 00:35:04 +0800

    捕获更多错误

--- src/helper/globals.ts
+++ src/helper/globals.ts
@@ -1,5 +1,18 @@
 export { }
 
+window.addEventListener('error', event => {
+
+    event.preventDefault()
+    console.warn(event.type, event.message)
+
+})
+
+window.addEventListener("rejectionhandled", event => {
+
+    console.warn(event.reason)
+
+}, false)
+
 window.addEventListener("unhandledrejection", event => {
 
     event.preventDefault()
