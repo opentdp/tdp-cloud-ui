8a907bb64bff928ee97b9bcf94a691d1ac883b56
熊 <qianyang@foxmail.com>
2023-02-07 17:35:38 +0800

    fix: 添加密钥，无法选中默认值

--- src/apps/terminal/keypair_create.vue
+++ src/apps/terminal/keypair_create.vue
@@ -64,7 +64,7 @@ export default class KeypairCreate extends Vue {
         <el-form ref="formRef" :model="formModel" :rules="formRules" label-width="50px">
             <el-form-item prop="KeyType" label="类型">
                 <el-select v-model="formModel.KeyType" placeholder="Select">
-                    <el-option v-for="k, v in KeypairTypeList" :key="k" :label="k" :value="v" />
+                    <el-option v-for="v, k in KeypairTypeList" :label="v" :value="Number(k)" />
                 </el-select>
             </el-form-item>
             <el-form-item prop="Description" label="别名">
