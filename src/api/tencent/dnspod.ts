5ed7e191c125a7a33cf07f265836e51273effced
若海 <wang@rehiy.com>
2023-02-08 14:43:17 +0800

    style: 命名冲突类型以I字母开头

--- src/api/tencent/dnspod.ts
+++ src/api/tencent/dnspod.ts
@@ -1,35 +1,35 @@
 import { TencentClient } from "./base"
-import { Dnspod as QDnspod } from "./typings"
+import { Dnspod as IDnspod } from "./typings"
 
 export class DnspodModel extends TencentClient {
     protected Service = "dnspod"
     protected Version = "2021-03-23"
 
-    public describeDomainList(query?: QDnspod.DescribeDomainListRequest): Promise<QDnspod.DescribeDomainListResponse> {
+    public describeDomainList(query?: IDnspod.DescribeDomainListRequest): Promise<IDnspod.DescribeDomainListResponse> {
         return this.bus({ Action: "DescribeDomainList", Payload: query })
     }
-    public describeRecordList(query: QDnspod.DescribeRecordListRequest): Promise<QDnspod.DescribeRecordListResponse> {
+    public describeRecordList(query: IDnspod.DescribeRecordListRequest): Promise<IDnspod.DescribeRecordListResponse> {
         return this.bus({ Action: "DescribeRecordList", Payload: query })
     }
 
-    public describeDomain(query?: QDnspod.DescribeDomainRequest): Promise<QDnspod.DescribeDomainResponse> {
+    public describeDomain(query?: IDnspod.DescribeDomainRequest): Promise<IDnspod.DescribeDomainResponse> {
         return this.bus({ Action: "DescribeDomain", Payload: query })
     }
 
-    public describeRecordType(query: QDnspod.DescribeRecordTypeRequest): Promise<QDnspod.DescribeRecordTypeResponse> {
+    public describeRecordType(query: IDnspod.DescribeRecordTypeRequest): Promise<IDnspod.DescribeRecordTypeResponse> {
         return this.bus({ Action: "DescribeRecordType", Payload: query }, 600)
     }
-    public describeRecordLineList(query: QDnspod.DescribeRecordLineListRequest): Promise<QDnspod.DescribeRecordLineListResponse> {
+    public describeRecordLineList(query: IDnspod.DescribeRecordLineListRequest): Promise<IDnspod.DescribeRecordLineListResponse> {
         return this.bus({ Action: "DescribeRecordLineList", Payload: query }, 600)
     }
 
-    public createRecord(query: QDnspod.CreateRecordRequest): Promise<QDnspod.CreateRecordResponse> {
+    public createRecord(query: IDnspod.CreateRecordRequest): Promise<IDnspod.CreateRecordResponse> {
         return this.bus({ Action: "CreateRecord", Payload: query })
     }
-    public modifyRecord(query: QDnspod.ModifyRecordRequest): Promise<QDnspod.ModifyRecordResponse> {
+    public modifyRecord(query: IDnspod.ModifyRecordRequest): Promise<IDnspod.ModifyRecordResponse> {
         return this.bus({ Action: "ModifyRecord", Payload: query })
     }
-    public deleteRecord(query: QDnspod.DeleteRecordRequest): Promise<QDnspod.DeleteRecordResponse> {
+    public deleteRecord(query: IDnspod.DeleteRecordRequest): Promise<IDnspod.DeleteRecordResponse> {
         return this.bus({ Action: "DeleteRecord", Payload: query })
     }
 }
--- src/api/tencent/tat.ts
+++ src/api/tencent/tat.ts
@@ -1,23 +1,23 @@
 import { TencentClient } from "./base"
-import { Tat as QTat } from "./typings"
+import { Tat as ITat } from "./typings"
 
 export class TatModel extends TencentClient {
     protected Service = "tat"
     protected Version = "2020-10-28"
 
-    public runCommand(region: string, query: QTat.RunCommandRequest): Promise<QTat.RunCommandResponse> {
+    public runCommand(region: string, query: ITat.RunCommandRequest): Promise<ITat.RunCommandResponse> {
         return this.bus({ Action: "RunCommand", Region: region, Payload: query })
     }
 
-    public describeCommands(region: string, query: QTat.DescribeCommandsRequest): Promise<QTat.DescribeCommandsResponse> {
+    public describeCommands(region: string, query: ITat.DescribeCommandsRequest): Promise<ITat.DescribeCommandsResponse> {
         return this.bus({ Action: "DescribeCommands", Region: region, Payload: query })
     }
 
-    public describeInvocations(region: string, query: QTat.DescribeInvocationsRequest): Promise<QTat.DescribeInvocationsResponse> {
+    public describeInvocations(region: string, query: ITat.DescribeInvocationsRequest): Promise<ITat.DescribeInvocationsResponse> {
         return this.bus({ Action: "DescribeInvocations", Region: region, Payload: query })
     }
 
-    public describeInvocationTasks(region: string, query: QTat.DescribeInvocationTasksRequest): Promise<QTat.DescribeInvocationTasksResponse> {
+    public describeInvocationTasks(region: string, query: ITat.DescribeInvocationTasksRequest): Promise<ITat.DescribeInvocationTasksResponse> {
         return this.bus({ Action: "DescribeInvocationTasks", Region: region, Payload: query })
     }
 }
