5bfb137b10fa198da393d28661229cdb4fc51fa9
荔枝小松鼠 <97099651@qq.com>
2023-02-09 20:09:04 +0800

    feat: 阿里云轻量服务器v0.1

--- src/api/alibaba/index.ts
+++ src/api/alibaba/index.ts
@@ -1,5 +1,11 @@
-import { AlibabaVendor } from "./base"
+import { TatModel } from '../tencent/tat';
+import { AlibabaVendor } from './base';
+import { EcsModel } from './ecs';
+import { SwasModel } from './swas';
 
 export default {
     vendor: AlibabaVendor,
-}
+    ecs: new EcsModel(),
+    swas: new SwasModel(),
+    tat: new TatModel(),
+};
---
+++ src/api/alibaba/swas.ts
@@ -0,0 +1,37 @@
+// import {  } from "./typings"
+import { AlibabaVendor, AlibabaClient } from './base';
+
+export class SwasModel extends AlibabaClient {
+    protected Service = 'swas';
+    protected Version = '2020-06-01';
+
+    public describeRegions(): Promise<any> {
+        return this.bus({ Action: 'ListRegions' }, 600);
+    }
+
+    public describeInstances(region: string, query?: any): Promise<any> {
+        query = Object.assign({ Limit: 100 }, query);
+        return this.bus({
+            Action: 'ListInstances',
+            RegionId: region,
+            Payload: {
+                ...query,
+                RegionId: region,
+            },
+        });
+    }
+}
+
+export const InstanceStateMap: Record<string, string> = {
+    PENDING: '创建中',
+    LAUNCH_FAILED: '创建失败',
+    RUNNING: '运行中',
+    STOPPED: '关机',
+    STARTING: '开机中',
+    STOPPING: '关机中',
+    REBOOTING: '重启中',
+    SHUTDOWN: '停止待销毁',
+    TERMINATING: '销毁中',
+    DELETING: '删除中',
+    FREEZING: '冻结中',
+};
