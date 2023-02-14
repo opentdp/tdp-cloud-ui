9f9af12039f51559fba2acf57159523ab5bb4058
若海 <wang@rehiy.com>
2023-02-14 21:03:53 +0800

    feat: 新增显示服务器信息

--- src/apps/dashboard/index.vue
+++ src/apps/dashboard/index.vue
@@ -1,6 +1,8 @@
 <script lang="ts">
 import { Component, Vue } from "vue-facing-decorator"
 
+import { EChartsOption } from "echarts"
+
 import { NaApi } from "@/api"
 import { UserSummary } from "@/api/native/passport"
 import { HostDetail } from "@/api/native/workhub"
@@ -11,11 +13,20 @@ import { bytesToSize } from "@/helper/format"
 export default class DashboardIndex extends Vue {
     public bytesToSize = bytesToSize
 
+    public timer = 0
+
     // 初始化
 
     public created() {
         this.getHostDetail()
         this.getUserSummary()
+        this.timer = setInterval(() => {
+            this.getHostDetail()
+        }, 5000)
+    }
+
+    public unmounted() {
+        clearInterval(this.timer)
     }
 
     // 系统信息
@@ -25,6 +36,15 @@ export default class DashboardIndex extends Vue {
     async getHostDetail() {
         const res = await NaApi.workhub.host()
         this.server = res
+        if (res.HostInfo.CpuPercent) {
+            this.setCpuUsedChartConfig(res.HostInfo.CpuPercent)
+        }
+        if (res.HostInfo.MemoryUsed) {
+            this.setMemUsedChartConfig(res.HostInfo.MemoryUsed, res.HostInfo.MemoryTotal)
+        }
+        if (res.HostInfo.DiskUsed) {
+            this.setDiskUsedChartConfig(res.HostInfo.DiskUsed, res.HostInfo.DiskTotal)
+        }
     }
 
     // 资源统计
@@ -35,11 +55,117 @@ export default class DashboardIndex extends Vue {
         const res = await NaApi.passport.summary()
         this.summary = res
     }
+
+    // CPU 实时负载
+
+    public cpuUsedChart!: EChartsOption
+
+    public setCpuUsedChartConfig(cpus: number[]) {
+        let value = 0
+        cpus.map(v => value += v)
+        this.cpuUsedChart = {
+            tooltip: {
+                formatter: '{a} <br/>{b} : {c}%'
+            },
+            series: [
+                {
+                    name: '处理器负载',
+                    type: 'gauge',
+                    progress: {
+                        show: true
+                    },
+                    detail: {
+                        valueAnimation: true,
+                        formatter: '{value}'
+                    },
+                    data: [
+                        {
+                            name: 'CPU',
+                            value: Math.round(value)
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+
+    // 内存实时用量
+
+    public memUsedChart!: EChartsOption
+
+    public setMemUsedChartConfig(used: number, total: number) {
+        const value = used / total * 100
+        this.memUsedChart = {
+            tooltip: {
+                formatter: '{a} <br/>{b} : {c}%'
+            },
+            series: [
+                {
+                    name: '内存用量',
+                    type: 'gauge',
+                    progress: {
+                        show: true
+                    },
+                    detail: {
+                        valueAnimation: true,
+                        formatter: '{value}'
+                    },
+                    data: [
+                        {
+                            name: 'Mem',
+                            value: Math.round(value)
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+
+    // 硬盘实时用量
+
+    public diskUsedChart!: EChartsOption
+
+    public setDiskUsedChartConfig(used: number, total: number) {
+        const value = used / total * 100
+        this.diskUsedChart = {
+            tooltip: {
+                formatter: '{a} <br/>{b} : {c}%'
+            },
+            series: [
+                {
+                    name: '硬盘用量',
+                    type: 'gauge',
+                    progress: {
+                        show: true
+                    },
+                    detail: {
+                        valueAnimation: true,
+                        formatter: '{value}'
+                    },
+                    data: [
+                        {
+                            name: 'Disk',
+                            value: Math.round(value)
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+
 }
 </script>
 
 <template>
     <div :loading="!server && !summary">
+        <el-card v-if="server">
+            <div class="chart-list">
+                <v-chart class="chart" :option="cpuUsedChart" />
+                <v-chart class="chart" :option="memUsedChart" />
+                <v-chart class="chart" :option="diskUsedChart" />
+            </div>
+        </el-card>
+        <div class="space-10" />
         <el-card v-if="summary" class="box-card">
             <template #header>
                 <div>资源统计</div>
@@ -113,6 +239,16 @@ export default class DashboardIndex extends Vue {
     b {
         color: var(--el-color-primary-dark-2);
         font-size: 2em;
+    }
+}
+
+.chart-list {
+    display: flex;
+    flex-wrap: wrap;
+
+    .chart {
+        width: 300px;
+        height: 300px;
     }
 }
 </style>
--- src/component/echarts.ts
+++ src/component/echarts.ts
@@ -16,7 +16,7 @@ import {
     ToolboxComponent,
     TooltipComponent,
 } from "echarts/components"
-import { BarChart, LineChart, PieChart } from "echarts/charts"
+import { BarChart, LineChart, GaugeChart, PieChart } from "echarts/charts"
 import { LabelLayout } from "echarts/features"
 import { CanvasRenderer } from "echarts/renderers"
 
@@ -31,6 +31,7 @@ use([
     // Chart
     BarChart,
     LineChart,
+    GaugeChart,
     PieChart,
     // Feature
     LabelLayout,
