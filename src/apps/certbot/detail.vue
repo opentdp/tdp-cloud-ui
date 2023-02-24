631f4b273c18b2551529c8e8ca241cbaa2163c3b
若海 <wang@rehiy.com>
2023-02-24 22:36:15 +0800

    fix: 修正证书格式

--- src/apps/certbot/detail.vue
+++ src/apps/certbot/detail.vue
@@ -26,10 +26,25 @@ export default class CertbotDetail extends Vue {
         const res = await NaApi.certbot.detail(id)
         this.certbot = res || {}
         if (res.Cert) {
-            this.crtText = res.Cert.Certificate.join("\n")
-            this.pubText = res.Cert.PrivateKey
+            this.crtText = this.crtFormat(res.Cert.Certificate)
+            this.pubText = atob(res.Cert.PrivateKey)
         }
         this.loading = false
+    }
+
+    private crtFormat(certs: string[]) {
+        const crtB = "-----BEGIN CERTIFICATE-----\n"
+        const crtE = "\n-----END CERTIFICATE-----\n"
+        certs = certs.map(c => this.split(c, 64).join("\n"))
+        return crtB + certs.join(`${crtE}\n${crtB}`) + crtE
+    }
+
+    private split(str: string, n: number) {
+        var arr = []
+        for (var i = 0, l = str.length; i < l / n; i++) {
+            arr.push(str.slice(n * i, n * (i + 1)))
+        }
+        return arr
     }
 }
 </script>
@@ -49,9 +64,9 @@ export default class CertbotDetail extends Vue {
         </el-breadcrumb>
         <div v-loading="loading" />
         <el-card>
-            <h3>Cert</h3>
+            <h3>Certificate</h3>
             <el-input v-model="crtText" type="textarea" rows="10" />
-            <h3>Key</h3>
+            <h3>PrivateKey</h3>
             <el-input v-model="pubText" type="textarea" rows="10" />
         </el-card>
     </div>
