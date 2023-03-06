7aeca27e6acd90f554dad6e16227fc45932f5061
若海 <wang@rehiy.com>
2023-03-06 22:52:33 +0800

    feat: 适配加密存储

--- src/api/native/terminal.ts
+++ src/api/native/terminal.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from "@/api/basic/http"
 
 export class TerminalModel extends HttpClient {
-    public getWebsshURL(rq: SSHRequest) {
+    public getWebsshURL(rq: SSHRequest, id = 0) {
+        if (rq.PrivateKey && /^\d+$/.test(rq.PrivateKey)) {
+            id = +rq.PrivateKey
+            delete rq.PrivateKey
+        }
         const origin = location.origin.replace(/^http/, "ws")
-        const wsurl = "/wsi/" + this.session.Token + "/terminal/ssh?"
-        const query = this.buildQuery(rq)
-        return origin + wsurl + query
+        const wsurl = "/wsi/" + this.session.Token + "/terminal/ssh/" + id
+        return origin + wsurl + "?" + this.buildQuery(rq)
     }
 }
 
--- src/apps/terminal/ssh_connect.vue
+++ src/apps/terminal/ssh_connect.vue
@@ -129,8 +129,7 @@ export default class TerminalSshConnect extends Vue {
         </el-form-item>
         <el-form-item v-if="authType == '2'" prop="PrivateKey" label="私玥">
             <el-select v-model="formModel.PrivateKey">
-                <el-option v-for="item in keypairList" :key="item.Id" :label="item.Description"
-                    :value="item.PrivateKey" />
+                <el-option v-for="v in keypairList" :key="v.Id" :label="v.Description" :value="v.Id" />
             </el-select>
         </el-form-item>
         <el-form-item v-if="authType == '4'" prop="PrivateKey" label="私钥">
