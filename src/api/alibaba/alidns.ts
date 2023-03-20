fcfebde44a763ce46d309acdd377114b7eb19726
若海 <wang@rehiy.com>
2023-03-20 18:02:38 +0800

    feat: 新增阿里域名管理页面

--- src/api/alibaba/alidns.ts
+++ src/api/alibaba/alidns.ts
@@ -5,38 +5,35 @@ export class AliDnsModel extends AlibabaClient {
     protected Service = 'alidns'
     protected Version = '2015-01-09'
 
-    public describeDomains(query?: Dns.DescribeDomainsRequest): Promise<Dns.DescribeDomainsResponseBody> {
+    public describeDomains(query?: Partial<Dns.DescribeDomainsRequest>): Promise<Dns.DescribeDomainsResponseBody> {
         return this.bus({ Action: 'DescribeDomains', Payload: query })
     }
 
-    public describeDomainRecords(query: Dns.DescribeDomainRecordsRequest): Promise<Dns.DescribeDomainRecordsResponseBody> {
-        return this.bus({ Action: 'DescribeDomainRecords', Payload: query })
+    public describeDomainRecords(query: Partial<Dns.DescribeDomainRecordsRequest>): Promise<Dns.DescribeDomainRecordsResponseBody> {
+        return this.bus({ Action: 'DescribeDomainRecords', Query: query })
     }
 
-    public describeDomainInfo(query?: Dns.DescribeDomainInfoRequest): Promise<Dns.DescribeDomainInfoResponseBody> {
-        return this.bus({ Action: 'DescribeDomainInfo', Payload: query })
+    public describeDomainInfo(query?: Partial<Dns.DescribeDomainInfoRequest>): Promise<Dns.DescribeDomainInfoResponseBody> {
+        return this.bus({ Action: 'DescribeDomainInfo', Query: query })
     }
 
-    public describeDomainRecordInfo(query: Dns.DescribeDomainRecordInfoRequest): Promise<Dns.DescribeDomainRecordInfoResponseBody> {
+    public describeDomainRecordInfo(query: Partial<Dns.DescribeDomainRecordInfoRequest>): Promise<Dns.DescribeDomainRecordInfoResponseBody> {
         return this.bus({ Action: 'DescribeDomainRecordInfo', Payload: query }, 600)
     }
 
-    public describeSupportLines(query: Dns.DescribeSupportLinesRequest): Promise<Dns.DescribeSupportLinesResponseBody> {
-        return this.bus(
-            { Action: 'DescribeSupportLines', Payload: query },
-            600
-        )
+    public describeSupportLines(query: Partial<Dns.DescribeSupportLinesRequest>): Promise<Dns.DescribeSupportLinesResponseBody> {
+        return this.bus({ Action: 'DescribeSupportLines', Payload: query }, 600)
     }
 
-    public addDomainRecord(query: Dns.AddDomainRecordRequest): Promise<Dns.AddDomainRecordResponseBody> {
-        return this.bus({ Action: 'AddDomainRecord', Payload: query })
+    public addDomainRecord(query: Partial<Dns.AddDomainRecordRequest>): Promise<Dns.AddDomainRecordResponseBody> {
+        return this.bus({ Action: 'AddDomainRecord', Query: query })
     }
 
-    public updateDomainRecord(query: Dns.UpdateDomainRecordResponse): Promise<Dns.UpdateAppKeyStateResponseBody> {
-        return this.bus({ Action: 'UpdateDomainRecord', Payload: query })
+    public updateDomainRecord(query: Partial<Dns.UpdateDomainRecordRequest>): Promise<Dns.UpdateDomainRecordResponseBody> {
+        return this.bus({ Action: 'UpdateDomainRecord', Query: query })
     }
 
-    public deleteDomainRecord(query: Dns.DeleteDomainRecordRequest): Promise<Dns.DeleteDomainGroupResponseBody> {
+    public deleteDomainRecord(query: Partial<Dns.DeleteDomainRecordRequest>): Promise<Dns.DeleteDomainGroupResponseBody> {
         return this.bus({ Action: 'DeleteDomainRecord', Payload: query })
     }
 }
@@ -45,4 +42,28 @@ export const DomainStatusMap: Record<string, string> = {
     ENABLE: '正常',
     PAUSE: '暂停',
     SPAM: '封禁',
+}
+
+export const RecordLineMap: Record<string, string> = {
+    default: "默认",
+    telecom: "电信",
+    unicom: "联通",
+    mobile: "移动",
+    oversea: "海外",
+    edu: "教育网",
+    drpeng: "鹏博士",
+    btvn: "广电网",
+}
+
+export const RecordTypeMap: Record<string, string> = {
+    A: "A 记录",
+    NS: "NS 记录",
+    MX: "MX 记录",
+    TXT: "TXT 记录",
+    CNAME: "CNAME 记录",
+    SRV: "SRV 记录",
+    AAAA: "AAAA 记录",
+    CAA: "CAA 记录",
+    REDIRECT_URL: "显性URL转发",
+    FORWARD_URL: "隐性URL转发",
 }
---
+++ src/provider/alibaba/dns/record_update.vue
@@ -0,0 +1,110 @@
+<script lang="ts">
+import { Ref, Component, Vue } from "vue-facing-decorator"
+
+import { FormInstanceFunctions, FormRules, SubmitContext, Data as TData } from "tdesign-vue-next"
+
+import Api, { AcApi } from "@/api"
+import { RecordLineMap, RecordTypeMap } from "@/api/alibaba/alidns"
+import * as AC from "@/api/alibaba/typings"
+
+@Component({
+    emits: ["submit"],
+    expose: ["open"],
+})
+export default class DnsRecordUpdate extends Vue {
+    public domainInfo!: AC.Dns.DescribeDomainInfoResponseBody
+
+    public recordType = RecordTypeMap
+    public recordLineList = RecordLineMap
+
+    // 创建表单
+
+    @Ref
+    public formRef!: FormInstanceFunctions
+
+    public formModel!: AC.Dns.DescribeDomainRecordsResponseBodyDomainRecordsRecord
+
+    public formRules: FormRules<AC.Dns.UpdateDomainRecordRequest> = {
+        RR: [{ required: true }],
+        Type: [{ required: true }],
+        Line: [{ required: true }],
+        Value: [{ required: true }],
+        TTL: [{ required: true }],
+        Priority: [{ required: true }],
+    }
+
+    // 提交表单
+
+    async formSubmit(ctx: SubmitContext<TData>) {
+        if (ctx.validateResult !== true) {
+            Api.msg.err("请检查表单")
+            return false
+        }
+        const query = {
+            RecordId: this.formModel.RecordId,
+            RR: this.formModel.RR,
+            Type: this.formModel.Type,
+            Line: this.formModel.Line,
+            Value: this.formModel.Value,
+            TTL: +this.formModel.TTL || 600,
+            Priority: +this.formModel.Priority || 1
+        }
+        await AcApi.alidns.updateDomainRecord(query)
+        this.close()
+    }
+
+    // 对话框管理
+
+    public visible = false
+
+    public close() {
+        this.visible = false
+        this.$emit("submit")
+    }
+
+    public open(info: AC.Dns.DescribeDomainInfoResponseBody, record: AC.Dns.DescribeDomainRecordsResponseBodyDomainRecordsRecord) {
+        this.visible = true
+        this.domainInfo = info
+        this.formModel = record
+    }
+}
+</script>
+
+<template>
+    <t-dialog v-model:visible="visible" destroy-on-close header="更新记录" :footer="false" width="400px">
+        <t-form ref="formRef" :data="formModel" :rules="formRules" label-width="80px" @submit="formSubmit">
+            <t-form-item name="RR" label="主机记录">
+                <t-input v-model="formModel.RR" />
+            </t-form-item>
+            <t-form-item name="Type" label="记录类型">
+                <t-select v-model="formModel.Type">
+                    <t-option v-for="v, k in recordType" :key="k" :value="k" :label="v" />
+                </t-select>
+            </t-form-item>
+            <t-form-item name="Line" label="线路类型">
+                <t-select v-model="formModel.Line">
+                    <t-option v-for="v, k in recordLineList" :key="k" :value="k" :label="v" />
+                </t-select>
+            </t-form-item>
+            <t-form-item name="Value" label="记录值">
+                <t-textarea v-model="formModel.Value" autosize />
+            </t-form-item>
+            <t-form-item v-if="formModel.Type == 'MX'" name="Priority" label="优先级">
+                <t-input-number v-model="formModel.Priority" />
+            </t-form-item>
+            <t-form-item name="TTL" label="TTL">
+                <t-input-number v-model="formModel.TTL" />
+            </t-form-item>
+            <t-form-item>
+                <t-space size="small">
+                    <t-button theme="primary" type="submit">
+                        提交
+                    </t-button>
+                    <t-button theme="default" @click="visible = false">
+                        取消
+                    </t-button>
+                </t-space>
+            </t-form-item>
+        </t-form>
+    </t-dialog>
+</template>
