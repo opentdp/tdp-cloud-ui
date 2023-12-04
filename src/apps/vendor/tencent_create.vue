edeed99f57ca11f281c1539c4294815beabcc8a3
若海 <wang@rehiy.com>
2023-12-04 14:00:16 +0800

    fix: 更正表单标题

--- src/apps/vendor/tencent_create.vue
+++ src/apps/vendor/tencent_create.vue
@@ -55,7 +55,7 @@ export default class VendorTencentCreate extends Vue {
 </script>
 
 <template>
-    <t-dialog v-model:visible="visible" destroy-on-close header="修改账号" :footer="false" width="50%">
+    <t-dialog v-model:visible="visible" destroy-on-close header="添加账号" :footer="false" width="50%">
         <t-form ref="formRef" :data="formModel" :rules="formRules" @submit="formSubmit">
             <t-form-item name="Description" label="别名">
                 <t-input v-model="formModel.Description" />
