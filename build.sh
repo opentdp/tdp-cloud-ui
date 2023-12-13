02c05445166cfd503321229f7710ddf24f11f846
若海 <wang@rehiy.com>
2023-12-13 12:07:01 +0800

    chore: 更新编译脚本

--- build.sh
+++ build.sh
@@ -4,6 +4,20 @@
 set -e
 set -o noglob
 
+####################################################################
+
+last_tag=`git tag | sort -V | tail -n 1`
+prev_tag=`git tag | sort -V | tail -n 2 | head -n 1`
+git log $last_tag..$prev_tag --pretty=format:"%s" | grep -v "^release" | sed 's/^/- /' | sort > RELEASE.md
+
+version=`echo $last_tag | sed 's/^v//'`
+sed -i "s/^const Version = \".*\"/const Version = \"$version\"/" src/helper/const.ts
+
+build_version=$((`grep -oP 'BuildVersion = "\K\d+' src/helper/const.ts` + ${GITHUB_RUN_NUMBER:-0}))
+sed -i "s/^const BuildVersion = \".*\"/const BuildVersion = \"$build_version\"/" src/helper/const.ts
+
+echo "build info - tag: $last_tag, version: $version, build: $build_version"
+
 ###########################################
 
 if [ ! -d node_modules ] ; then
