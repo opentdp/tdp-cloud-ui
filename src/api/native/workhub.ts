2a83e67bf9bcd3c5fa77e37ac2adb331d44f677c
若海 <wang@rehiy.com>
2023-12-15 23:32:14 +0800

    fix: 防止重复修改标题

--- src/api/native/workhub.ts
+++ src/api/native/workhub.ts
@@ -20,7 +20,7 @@ export class WorkhubModel extends HttpClient {
     }
 
     public exec(id: string, rq: ScriptPayload): Promise<{ Id: number }> {
-        rq.Name = 'Exec: ' + rq.Name;
+        rq = { ...rq, Name: 'Exec: ' + rq.Name };
         return this.post('/workhub/exec/' + id, rq);
     }
 
