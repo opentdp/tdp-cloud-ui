413b4f521e53340456b2417681fc0688ab8bb4bf
若海 <wang@rehiy.com>
2023-12-16 09:23:12 +0800

    chore: 修复版本自增

--- build.sh
+++ build.sh
@@ -13,10 +13,10 @@ prev_tag=`git tag | sort -V | tail -n 2 | head -n 1`
 git log $prev_tag..$last_tag --pretty=format:"%s" | grep -v "^release" | sed 's/^/- /' | sort > RELEASE.md
 
 version=`echo $last_tag | sed 's/^v//'`
-sed -i "s/^const Version = \".*\"/const Version = \"$version\"/" src/helper/const.ts
+sed -i "s/const Version = '.*'/const Version = '$version'/" src/helper/const.ts
 
 build_version=$((`grep -oP "BuildVersion = '\K\d+" src/helper/const.ts` + $RUN_NUMBER))
-sed -i "s/^const BuildVersion = \".*\"/const BuildVersion = \"$build_version\"/" src/helper/const.ts
+sed -i "s/const BuildVersion = '.*'/const BuildVersion = '$build_version'/" src/helper/const.ts
 
 echo "build info - tag: $last_tag, version: $version, build: $build_version"
 
