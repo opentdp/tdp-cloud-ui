63edf14b8f10cdfcdab9bbfcb58c28125290b9b4
若海 <wang@rehiy.com>
2023-12-26 21:03:55 +0800

    feat: 允许不指定解释器

--- src/apps/script/create.vue
+++ src/apps/script/create.vue
@@ -33,10 +33,6 @@ export default class ScriptCreate extends Vue {
             Api.msg.err('请检查表单');
             return false;
         }
-        if (this.formModel.CommandType == 'SHELL' && this.formModel.Content.indexOf('#!/') !== 0) {
-            Api.msg.err('请在首行声明解释器，如 #!/bin/sh');
-            return false;
-        }
         await NaApi.script.create(this.formModel);
         this.close();
     }
@@ -48,9 +44,6 @@ export default class ScriptCreate extends Vue {
             case 'SHELL':
                 this.formModel.Username = 'root';
                 this.formModel.WorkDirectory = '/root';
-                if (this.formModel.Content.indexOf('#!/') !== 0) {
-                    this.formModel.Content = '#!/bin/sh\n' + this.formModel.Content;
-                }
                 break;
             case 'POWERSHELL':
             case 'BAT':
@@ -105,7 +98,7 @@ export default class ScriptCreate extends Vue {
                 <t-input v-model="formModel.WorkDirectory" />
             </t-form-item>
             <t-form-item name="Content" label="脚本内容">
-                <t-textarea v-model="formModel.Content" :autosize="{ minRows: 4, maxRows: 15 }" :placeholder="formModel.CommandType == 'SHELL' ? '请在首行声明解释器，如 #!/bin/sh' : ''" />
+                <t-textarea v-model="formModel.Content" :autosize="{ minRows: 4, maxRows: 15 }" :placeholder="formModel.CommandType == 'SHELL' ? '#!/bin/sh' : ''" />
             </t-form-item>
             <t-form-item name="Description" label="脚本描述">
                 <t-textarea v-model="formModel.Description" :autosize="{ minRows: 2, maxRows: 8 }" />
