b15cba859d71bb1c188b8fd76664548a153a3ecc
若海 <wang@rehiy.com>
2023-12-28 14:42:24 +0800

    fix: 适配新版接口路由

--- src/api/native/workhub.ts
+++ src/api/native/workhub.ts
@@ -3,31 +3,32 @@ import { HttpClient } from '@/api/basic/http';
 import { GoMemoryStat, SummaryStat, DetailStat, FilerRequest, FilerResponse, ScriptPayload } from './typings';
 
 export class WorkhubModel extends HttpClient {
-    public list(): Promise<WorkerPaged> {
-        return this.post('/workhub/list', {});
-    }
-
-    public detail(id: string): Promise<NodeDetail> {
-        id = id && id != 'host' ? '/' + id : '';
-        return this.post('/workhub/detail' + id, {});
-    }
-
-    public exec(id: string, rq: ScriptPayload): Promise<{ Id: number }> {
-        id = id && id != 'host' ? '/' + id : '';
-        rq = { ...rq, Name: 'Exec: ' + rq.Name };
-        return this.post('/workhub/exec' + id, rq);
-    }
-
-    public filer(id: string, rq: FilerRequest): Promise<FilerResponse> {
-        id = id && id != 'host' ? '/' + id : '';
-        rq.Path = rq.Path.replace(/\\+/g, '/').replace(/\/+/g, '/');
-        return this.post('/workhub/filer' + id, rq);
-    }
 
     public getRegisterURL() {
         const origin = location.origin.replace(/^http/, 'ws');
         const wsurl = '/wsi/' + this.session.AppId + '/workhub';
         return origin + wsurl;
+    }
+
+    public list(): Promise<WorkerPaged> {
+        return this.post('/workhub/list', {});
+    }
+
+    public detail(id: string): Promise<NodeDetail> {
+        id = id == 'host' ? '/workhub' : '/worker/' + id;
+        return this.post(id + '/detail', {});
+    }
+
+    public exec(id: string, rq: ScriptPayload): Promise<{ Id: number }> {
+        id = id == 'host' ? '/workhub' : '/worker/' + id;
+        rq = { ...rq, Name: 'Exec: ' + rq.Name };
+        return this.post(id + '/exec', rq);
+    }
+
+    public filer(id: string, rq: FilerRequest): Promise<FilerResponse> {
+        id = id == 'host' ? '/workhub' : '/worker/' + id;
+        rq.Path = rq.Path.replace(/\\+/g, '/').replace(/\/+/g, '/');
+        return this.post(id + '/filer', rq);
     }
 }
 
